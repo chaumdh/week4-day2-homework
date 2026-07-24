e74592cb0be1df55e97acff01b88b188751d859b
chaumdh <chauhuynh.0997@gmail.com>
2026-07-23 17:10:46 -0700

    complete disableButton

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,11 +1,13 @@
 /* ------ RANDOM CODES ------ */
 
+// Create variables to store generated codes and the type of characters we want to show as codes
+var code = ''; // initialize to null value
+var getCode = ''; // to store entered code
+var btnvalue; // for button boolean value
+var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+
 // Function to generate combination of characters
 function generateCode() {
-    // Create variables to store generated codes and the type of characters we want to show as codes
-    var code = ''; // initialize to null value
-    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
-
     // Generate character multiple times using a loop
     for (i = 1; i <= 8; i++) {
         var char = Math.random()* str.length; // random select a character from the variable str
@@ -17,11 +19,17 @@ function generateCode() {
 // Get HTML element to display
 document.getElementById("codes").innerHTML = generateCode();
 
-// Disable Button
-function disableButton() {
-    document.getElementById("submit").disabled = true;
+// determine when to enable or disable button
+function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue; // enable/disable button
+    if (btnvalue == true) { // test if button is disabled or enabled
+        // set button and label color translucent
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        // set button and label color with no transparency
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
+    console.log("button disabled");
 }
-
-// Activate function
-disableButton();
-
